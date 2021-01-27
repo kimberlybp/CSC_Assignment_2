33c75b53948992bab3d6dc1bd35e4f318ba177fb
Wei Xian <wxian20@gmail.com>
2021-01-27 20:08:25 +0800

    WIP

--- server.js
+++ server.js
@@ -27,6 +27,74 @@ app.get('/subscribe', function (req, res) {
     res.sendFile(filePath);
 });
 
+app.post('/customer-portal', async (req, res) => {
+    const session = await stripe.billingPortal.sessions.create({
+        customer: '',
+        return_url: 'http://localhost:3000/index.html',
+    });
+    sessionURL = session.url;
+    res.redirect(session.url);
+});
+
+app.get('/checkout-session', async (req, res) => {
+    const { sessionId } = req.query;
+    const session = await stripe.checkout.sessions.retrieve(sessionId);
+    res.send(session);
+});
+
+app.post('/create-checkout-session', async (req, res) => {
+    const domainURL = process.env.DOMAIN;
+    const { priceId } = req.body;
+
+    try {
+        const session = await stripe.checkout.sessions.create({
+            customer: 'cus_IfjppW291BfofB',
+            mode: 'subscription',
+            payment_method_types: ['card'],
+            line_items: [
+                {
+                    price: priceId,
+                    quantity: 1,
+                },
+            ],
+            // ?session_id={CHECKOUT_SESSION_ID} means the redirect will have the session ID set as a query param
+            success_url: `${domainURL}/success.html?session_id={CHECKOUT_SESSION_ID}`,
+            cancel_url: `${domainURL}/canceled.html`,
+        });
+
+        res.send({
+            sessionId: session.id,
+        });
+    } catch (e) {
+        res.status(400);
+        return res.send({
+            error: {
+                message: e.message,
+            },
+        });
+    }
+});
+
+app.post('/customer-portal-byId', async (req, res) => {
+    const { customerId } = req.body;
+
+    const portalsession = await stripe.billingPortal.sessions.create({
+        customer: customerId,
+        return_url: process.env.DOMAIN,
+    });
+
+    res.send({
+        url: portalsession.url,
+    });
+});
+
+app.get('/setup', (req, res) => {
+    res.send({
+        publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+        proPrice: process.env.PREMIUM_PRICE_ID,
+    });
+});
+
 // App configs
 const server = http.createServer(app);
 const port = 3000;
--- src/script.js
+++ src/script.js
@@ -18,17 +18,17 @@ var handleFetchResult = function (result) {
 };
 
 // Create a Checkout Session with the selected plan ID
-var createCheckoutSession = function (priceId, customerId) {
-    return fetch('/create-checkout-session', {
+var createCheckoutSession = async function (priceId, customerId) {
+    const result = await fetch('/create-checkout-session', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({
             priceId: priceId,
-            customerId: customerId,
         }),
-    }).then(handleFetchResult);
+    });
+    return handleFetchResult(result);
 };
 
 // Handle any errors returned from Checkout
@@ -45,3 +45,62 @@ var showErrorMessage = function (message) {
 };
 
 /* Get your Stripe publishable key to initialize Stripe.js */
+fetch('/setup')
+    .then(handleFetchResult)
+    .then(function () {
+        var publishableKey =
+            'pk_test_51Hx6udF6nia1fA2P8wOMf1eodbJIt7gYStqUoDYJQjeTB97PPXmNPRjAHNkUyIot3x10XPIvWfNmbRg7ysMuusZV008fphPWNk';
+        var premiumPriceId = 'price_1ICnFzF6nia1fA2PrjjPHyEA';
+        var customerId = '123';
+        var customerEmail = 'abc.com';
+
+        var stripe = Stripe(publishableKey);
+
+        // Setup event handler to create a Checkout Session when button is clicked
+        document.getElementById('subscription-plan-btn').addEventListener('click', function (evt) {
+            document.getElementById('loading-overlay').style.display = 'block';
+            createCheckoutSession(premiumPriceId, customerId).then(function (data) {
+                // Call Stripe.js method to redirect to the new Checkout page
+                stripe
+                    .redirectToCheckout({
+                        sessionId: data.sessionId,
+                    })
+                    .then(handleResult);
+            });
+        });
+
+        if (customerEmail) {
+            document.getElementById('welcome').innerHTML = 'Welcome ' + customerEmail + '!';
+        } else {
+            document.getElementById('welcome').innerHTML = 'Welcome guest user!';
+            document.getElementById('logout').innerHTML = 'Back to Login';
+        }
+
+        const manageBillingForm = document.querySelector('#manage-billing-form');
+        manageBillingForm.addEventListener('submit', function (e) {
+            e.preventDefault();
+            document.getElementById('loading-overlay').style.display = 'block';
+            fetch('/customer-portal-byId', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    customerId: customerId,
+                }),
+            })
+                .then((response) => response.json())
+                .then((data) => {
+                    window.location.href = data.url;
+                })
+                .catch((error) => {
+                    console.error('Error:', error);
+                });
+        });
+
+        if (customerId == null) {
+            document.getElementById('manage').disabled = true;
+        }
+
+        document.getElementById('loading-overlay').style.display = 'none';
+    });
