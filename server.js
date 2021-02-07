60bae6d819d2f4b699489c9fa09bfb073f685ed2
Kimberly <kimberlybp19.18@ichat.sp.edu.sg>
2021-02-07 09:32:12 +0800

    test

--- server.js
+++ server.js
@@ -211,6 +211,6 @@ function createDateTimeString(timestamp) {
 
 // App configs
 const server = http.createServer(app);
-const port = 3000;
+const port = process.env.PORT || 3000;
 server.listen(port);
 console.debug('Server listening on port ' + port);
